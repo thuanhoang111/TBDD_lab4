cadb6ef974a0fb60bd7d998cb1f6bd59169c0e15
Thuan Hoang <thamthuy100272@gmail.com>
2022-03-14 22:54:43 +0700

    first commit

---
+++ lab4/app/src/main/java/com/example/lab4/Product.java
@@ -0,0 +1,37 @@
+package com.example.lab4;
+
+public class Product {
+    private String name;
+    private String description;
+    private int picture;
+
+    public Product(String name, String description, int picture) {
+        this.name = name;
+        this.description = description;
+        this.picture = picture;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getPicture() {
+        return picture;
+    }
+
+    public void setPicture(int picture) {
+        this.picture = picture;
+    }
+}
